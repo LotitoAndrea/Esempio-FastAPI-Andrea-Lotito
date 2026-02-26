a37570454165cc12c2aed830f3879aff461eac09
LotitoAndrea <lotito.andrea@itisgrassi.edu.it>
2026-02-26 14:46:21 +0000

    aggiunto patch all'esempio di nome finendo le op. CRUD

--- main.py
+++ main.py
@@ -21,6 +21,15 @@ def post_nome(nomeReq: NomePostReq):
     nomi.append(nomeReq.nome)
     return nomeReq
 
+@app.patch("/nome/{nomeVecchio}/{nomeNuovo}")
+def patch_nome(nomeVecchio: str, nomeNuovo: str):
+    if nomeVecchio not in nomi:
+        raise HTTPException(status_code=404, detail="nome non trovato.")
+    
+    nomi.remove(nomeVecchio)
+    nomi.append(nomeNuovo)
+    return "nome vecchio '"+ nomeVecchio +"', sovrascritto con nome nuovo '"+ nomeNuovo + "'."
+
 @app.delete("/nome/{nome}")
 def delete_nome(nome: str):
     if nome not in nomi:
